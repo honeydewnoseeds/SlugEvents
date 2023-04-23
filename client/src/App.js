8d38ccb533dd888132df7d7ee966c8e3407855bc
amuralitharan <avneesh.muralitharan@gmail.com>
2023-04-23 16:53:32 -0700

    read from database

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
 import logo from './logo.svg';
 import './App.css';
+import { db } from './config/firebase';
+import { getDocs, collection } from "firebase/firestore";
 
 function App() {
+  const [eventList, setEventList] = useState([]);
+
+  const moviesCollectionRef = collection(db, "events");
+  useEffect(() => {
+    const getEventList = async() => {
+      try {
+        const data = await getDocs(moviesCollectionRef);
+        console.log(data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    getEventList();
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
