70b94678462b658165a35add3195baf7e5ba1df2
Edison <ediesonchen@gmail.com>
2023-04-30 15:35:39 -0700

    button customization

--- client/src/Components/filters.js
+++ client/src/Components/filters.js
@@ -3,22 +3,22 @@ import React from "react";
 function Filters({ handleFilterChange }) {
   return (
     <div className="btns">
-      <button value="All" onClick={handleFilterChange}>
+      <button class = "button btn1" value="All" onClick={handleFilterChange}>
         All
       </button>
-      <button value="College 9, College 10, College 9/10" onClick={handleFilterChange}>
+      <button class = "button btn2" value="College 9, College 10, College 9/10" onClick={handleFilterChange}>
         College 9/10
       </button>
-      <button value="Cowell, Stevenson" onClick={handleFilterChange}>
+      <button class = "button btn3" value="Cowell, Stevenson" onClick={handleFilterChange}>
         Cowell/Stevenson
       </button>
-      <button value="Porter, Kresge" onClick={handleFilterChange}>
+      <button class = "button btn4" value="Porter, Kresge" onClick={handleFilterChange}>
         Porter/Kresge
       </button>
-      <button value="Oakes, RCC" onClick={handleFilterChange}>
+      <button class = "button btn5" value="Oakes, RCC" onClick={handleFilterChange}>
         Oakes/RCC
       </button>
-      <button value="Crown, Merill" onClick={handleFilterChange}>
+      <button class = "button btn6" value = "Crown, Merill" onClick={handleFilterChange}>
         Crown/Merill
       </button>
     </div>
