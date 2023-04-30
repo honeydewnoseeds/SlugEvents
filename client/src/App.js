c1bfe8dd2b477ddde5250713c2ac9f5ec3f2f270
Edison <ediesonchen@gmail.com>
2023-04-30 16:46:56 -0700

    merge changes from alex

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,99 @@
+import { useEffect, useState } from "react";
+import logo from './logo.svg';
+import './App.css';
+import { db } from './config/firebase';
+import { getDocs, collection } from "firebase/firestore";
+import { addDoc } from "firebase/firestore";
 import Landing from "./Landing";
 import "./App.css";
 import { ThemeProvider } from "@mui/material";
 import { themeOptions } from "./Components/theme";
-
-//
-import { createStyles, Grid, makeStyles, Paper } 
-    from '@mui/material';
-import React from 'react';
-
+import { deleteDoc, doc, writeBatch } from "firebase/firestore";
+import { onSnapshot } from "firebase/firestore";
 
 function App() {
-  return (  
-  <ThemeProvider theme={themeOptions}>
-    <Landing />
-  </ThemeProvider>
+  const [eventList, setEventList] = useState([]);
+  const [filteredEventList, setFilteredEventList] = useState(null);
+  const eventsCollectionRef = collection(db, "events");
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(eventsCollectionRef, (snapshot) => {
+      const updatedData = snapshot.docs.map((doc) => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setEventList(updatedData);
+    });
+  
+    // Clean up the listener when the component unmounts
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  // Define a function to filter events by the "account" field
+  const filterByAccount = (accountToFilter) => {
+    const filteredEvents = eventList.filter((event) => event.account === accountToFilter);
+    setFilteredEventList(filteredEvents);
+  };
+
+  // Attach the filterByAccount function to the window object
+  useEffect(() => {
+    window.filterByAccount = filterByAccount;
+    return () => {
+      // Clean up the function from the window object when the component is unmounted
+      delete window.filterByAccount;
+    };
+  }, [eventList]);
+
+  const resetFilter = () => {
+    setFilteredEventList(null);
+  };
+
+  // Attach the resetFilter function to the window object
+  useEffect(() => {
+    window.resetFilter = resetFilter;
+    return () => {
+      // Clean up the function from the window object when the component is unmounted
+      delete window.resetFilter;
+    };
+  }, []);
+
+//Add an event using the format addEvent({account: "Porter/Kresge", description: "Lorem ipsum", imageSrc: "https://source.unsplash.com/random"})
+const addEvent = async (event = {
+  account: "Porter/Kresge",
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  imageSrc: "https://source.unsplash.com/random"
+}) => {
+  try {
+    const docRef = await addDoc(eventsCollectionRef, event);
+    console.log("Event added with ID:", docRef.id);
+  } catch (err) {
+    console.error("Error adding event:", err);
+  }
+};
+  window.addEvent = addEvent;
+  
+  //Clears all events from database
+  const clearEvents = async () => {
+    try {
+      const data = await getDocs(eventsCollectionRef);
+      const batch = writeBatch(db);
+      data.docs.forEach((docSnapshot) => {
+        batch.delete(doc(eventsCollectionRef, docSnapshot.id));
+      });
+      await batch.commit();
+      console.log("All events deleted");
+    } catch (err) {
+      console.error("Error deleting events:", err);
+    }
+  };
+  window.clearEvents = clearEvents;
+
+  return (
+    <ThemeProvider theme={themeOptions}>
+      <Landing eventList={filteredEventList || eventList} />
+    </ThemeProvider>
   );
 }
 
---
+++ client/src/config/firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getFirestore} from "firebase/firestore"
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Stuff
+const firebaseConfig = {
+  apiKey: "AIzaSyBs5ohFQ2veZgcrNCUJn60JqzEcuQE2lAw",
+  authDomain: "slugevents-57e0b.firebaseapp.com",
+  projectId: "slugevents-57e0b",
+  storageBucket: "slugevents-57e0b.appspot.com",
+  messagingSenderId: "819769300151",
+  appId: "1:819769300151:web:7bf1ddc0b5bcea5fbb2932",
+  measurementId: "G-RD630CEKKR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const db = getFirestore(app);
