baa7c1a62d0c9bcc06269c33ab69432e6284b88d
amuralitharan <avneesh.muralitharan@gmail.com>
2023-05-07 12:21:01 -0700

    Update scraper.py
    Scraper works

--- scraper.py
+++ scraper.py
@@ -1,36 +1,80 @@
 import os
-from instagrapi import Client
-from google.cloud import firestore
+import instaloader
+from datetime import datetime
+from firebase_admin import credentials, initialize_app, storage, firestore
 
-# Firestore setup
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'slugevents-57e0b-firebase-adminsdk-brd1a-dcc2a15087.json'
-db = firestore.Client()
+# Initialize Firebase Admin SDK
+cred = credentials.Certificate("slugevents-57e0b-firebase-adminsdk-brd1a-dcc2a15087.json")
+initialize_app(cred, {
+    "storageBucket": "slugevents-57e0b.appspot.com"
+})
 
-# Instagram login
-username = "slugevents"  # Replace with your username
-password = "slugslugslug"  # Replace with your password
+# Initialize Instaloader
+L = instaloader.Instaloader()
 
-cl = Client()
-cl.login(username, password)
+# Instagram profiles to download images from
+profiles = [
+    "ucsc9_jrl",
+    "porter.college",
+    "kc_ucsc",
+    "stevenson.ucsc",
+    "cowell.ucsc",
+    "rcc_ucsc",
+    "oakescollege",
+    "ucsccrowncollege",
+    "ucscmerillcollege"
+]
 
-user_id = cl.user_id_from_username("cowell.ucsc")
-medias = cl.user_medias(user_id, 20)
+# Firebase Storage and Firestore clients
+bucket = storage.bucket()
+db = firestore.client()
 
-for media in medias:
-    # Create a document with media ID as its identifier
-    doc_ref = db.collection("events").document(str(media.pk))
+# Create 'instagram_images' directory if it doesn't exist
+os.makedirs("instagram_images", exist_ok=True)
 
-    # Prepare the data to be stored in Firestore
-    data = {
-        "account": "cowell.ucsc",
-        "imageSrc": media.thumbnail_url,
-        "description": media.caption_text,
-        "date_posted": media.taken_at,
-    }
+# Download and process Instagram images
+for profile_name in profiles:
+    profile = instaloader.Profile.from_username(L.context, profile_name)
 
-    # Store the data in Firestore
-    doc_ref.set(data)
+    for post in profile.get_posts():
+        post_id = post.shortcode  # Use the post's shortcode as a unique identifier
 
-    # Print the data for debugging purposes
-    print(data)
-    print("------")
+        # Check if the post already exists in the Firestore database
+        existing_post = db.collection("events").document(post_id).get()
+        if existing_post.exists:
+            print(f"Skipping duplicate post: {post_id}")
+            continue
+
+        # Check if the post is a sidecar (album)
+        if post.typename == "GraphSidecar":
+            sidecar_nodes = list(post.get_sidecar_nodes())  # Convert generator to list
+            url = sidecar_nodes[0].display_url  # Get the URL of the first image
+        else:
+            url = post.url
+
+        # Download image
+        image_name = f"{profile_name}_{post.date_utc.strftime('%Y%m%d_%H%M%S')}"
+        image_path = os.path.join("instagram_images", image_name)
+        L.download_pic(image_path, url, post.date_utc)
+
+        # Append the file extension to the image_path
+        image_path += ".jpg"
+
+        # Upload image to Firebase Storage
+        blob = bucket.blob(f"{image_name}.jpg")
+        blob.upload_from_filename(image_path)
+        blob.make_public()
+
+        # Get image URL from Firebase Storage
+        image_url = blob.public_url
+        
+        # Store image metadata in Firestore
+        doc_ref = db.collection("events").document(post_id)
+        doc_ref.set({
+            "account": profile_name,
+            "description": post.caption,
+            "date_posted": post.date_utc,
+            "imageSrc": image_url
+        })
+
+        print(f"Processed {image_name}.jpg")
