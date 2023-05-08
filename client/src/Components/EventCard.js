7f586d684cb44c1a4cc726d4d6db1d9675eb9829
Edison <ediesonchen@gmail.com>
2023-05-08 09:20:57 -0700

    event card styling

--- client/src/Components/EventCard.js
+++ client/src/Components/EventCard.js
@@ -1,15 +1,10 @@
 import React from "react";
-import { Card, CardContent, Stack, Typography } from "@mui/material";
+import { Card, CardContent, Grid, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
-// Props.title = Event Title
-// Props.description = event description
-// Props.imageSrc = flyer from event
-// Props.account = account that posted the event
 const Event = (props) => {
   const navigate = useNavigate();
 
-  //write a function that assigns a color to the card based on the college
   const color = () => {
     if (props.account === "ucsc9_jrl") {
       return "#E27396";
@@ -38,45 +33,40 @@ const Event = (props) => {
   };
 
   return (
-    <Stack spacing={2} direction="column" alignItems="center">
-      <Card
-        sx={{
-          width: "90vw",
-          backgroundColor: color(props.account),
-          borderRadius: "16px",
+    <Card
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        width: "auto",
+        backgroundColor: color(props.account),
+        borderRadius: "16px",
+      }}
+      onClick={() => navigate("/info")}
+    >
+      <img
+        src={props.imageSrc}
+        alt={props.title}
+        style={{
+          width: "30%",
+          height: "auto",
+          objectFit: "cover",
+          borderRadius: "16px 0 0 16px",
         }}
-        onClick={() => navigate("/info")}
-      >
-        <Stack direction="column" alignItems="center" textAlign="center">
-          <CardContent>
-            <Typography
-              variant="h4"
-              sx={{ textAlign: "center", color: "primary.main" }}
-            >
+      />
+      <CardContent>
+        <Grid container direction="column" spacing={2}>
+          <Grid item>
+            <Typography variant="h4" color="primary">
               {props.title}
             </Typography>
-            <img
-              src={props.imageSrc}
-              alt={props.title}
-              style={{
-                width: "100%",
-                height: "auto",
-                margin: "0.1rem",
-                marginBottom: "0.5rem",
-                objectFit: "cover",
-                borderRadius: "16px",
-              }}
-            />
-            {/* </Card> */}
-            <Typography
-              sx={{ textAlign: "left", fontSize: "h3", noWrap: "false" }}
-            >
-              {props.description}
-            </Typography>
-          </CardContent>
-        </Stack>
-      </Card>
-    </Stack>
+          </Grid>
+          <Grid item>
+            <Typography variant="body1">{props.description}</Typography>
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
   );
 };
 
