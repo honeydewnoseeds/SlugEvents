9d59309d8c475b5d73ef03eac151e93238770cd2
yunqing liang <yliang82@ucsc.edu>
2023-05-21 19:50:21 -0700

    added a slider view of different components

--- client/src/pages/Info.js
+++ client/src/pages/Info.js
@@ -1,19 +1,60 @@
 import { Stack, Button, Box, CardContent, IconButton, Grid} from "@mui/material";
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
 import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import { useTheme } from '@mui/material/styles';
 import Card from '@mui/material/Card';
-import CardMedia from '@mui/material/CardMedia';
+import MobileStepper from '@mui/material/MobileStepper';
 import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import { useLocation, useNavigate } from "react-router-dom";
-
+import * as React from 'react';
 
 function Board() {
   const navigate = useNavigate()
   const location = useLocation();
   let imageSrc = location.state.imageSrc;
   let description = location.state.description;
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };    
+
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const maxSteps = 2;
+
+  const slider = (current) => {
+    if (current===0){
+      return (    
+      <Grid container direction="column" alignItems="center" justify="center">
+              <img
+                src= {imageSrc}
+                alt= "titlee"
+                style={{
+                  width: "70vw",
+                  height: "73vh", // Set a fixed height for the images
+                  objectFit: "cover",
+                  borderRadius: "16px 16px 16px 16px",
+                }}
+            />
+              </Grid>
+              
+      );          
+    }else{
+        return( 
+          <Typography variant= "h6">
+              {description}
+          </Typography>
+        );    
+    }
+  }
 
   return (
     <>
@@ -39,30 +80,43 @@ function Board() {
     </Grid>
 
     <Grid container direction="column" alignItems="center" justify="center">
-      <Card variant="outlined" sx= {{border:"primary.main", borderRadius: '16px', width: "80vw", height: "78vh"}}>
-        <CardContent>
-          <Grid container direction="column" alignItems="center" justify="center">
-          <img
-            src= {imageSrc}
-            alt= "titlee"
-            style={{
-              width: "30%",
-              height: "100%", // Set a fixed height for the images
-              objectFit: "cover",
-              borderRadius: "16px 16px 16px 16px",
-            }}
-        />
-          </Grid>
-          <Divider />
-          <Typography variant="h5" component={'div'}>
-            Events Specification: 
-            <Typography variant= "h6">
-              {description}
-            </Typography>
-          </ Typography>
-        </CardContent>
+    <Card variant="outlined" sx= {{border:"primary.main", borderRadius: '16px', width: "80vw", height: "78vh"}}>
+      <CardContent>
+        {slider(activeStep)}
+      </CardContent>
       </Card>
     </Grid>
+  
+    <MobileStepper
+        steps={maxSteps}
+        position="static"
+        activeStep={activeStep}
+        nextButton={
+          <Button
+            size="small"
+            onClick={handleNext}
+            disabled={activeStep === maxSteps - 1}
+          >
+            Next
+            {theme.direction === 'rtl' ? (
+              <KeyboardArrowLeft />
+            ) : (
+              <KeyboardArrowRight />
+            )}
+          </Button>
+        }
+        backButton={
+          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+            {theme.direction === 'rtl' ? (
+              <KeyboardArrowRight />
+            ) : (
+              <KeyboardArrowLeft />
+            )}
+            Back
+          </Button>
+        }
+      />
+
      <Stack direction= "row" spacing={15}>
         <Button variant="contained" startIcon={<CalendarMonthIcon />}>
           Calendar
