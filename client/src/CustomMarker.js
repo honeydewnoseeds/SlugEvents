e36accf446472618b7cfdf5f56567d189772e670
Derek Morimoto <dmorimot@ucsc.edu>
2023-05-31 13:50:01 -0700

    adding map.js and CustomMarker.js with both working and with comments

--- client/src/CustomMarker.js
+++ client/src/CustomMarker.js
@@ -1,34 +1,48 @@
-import React, { useEffect } from 'react';
+import {useEffect} from 'react';
 
-const CustomMarker = ({ google, map, locationName, description }) => {
+////
+// Function which exports a Marker given a, Event Name, Location Name and Time
+// Uses google and map from map.js so they don't have to be imported into this file
+////
+const CustomMarker = ({google, map, eventName, locationName, time}) => {
   useEffect(() => {
+    // Geocoder imported from the google maps api
     const geocoder = new google.maps.Geocoder();
 
-    geocoder.geocode({ address: locationName }, (results, status) => {
-      if (status === 'OK' && results && results.length > 0) {
-        const { geometry } = results[0];
-        const { location } = geometry;
+    // Random offset generated for when same locations are used
+    const offset = Math.random() * 0.0001;
 
+    // Uses the Location name in prder to get lat, lng for marker
+    geocoder.geocode({address: locationName}, (results, status) => {
+      if (status === 'OK' && results && results.length > 0) {
+        const {geometry} = results[0];
+        const {location} = geometry;
+
+        // Sets location for Marker as well as Title
         const marker = new google.maps.Marker({
           map,
-          position: location,
-          title: locationName,
+          position: {
+            lat: location.lat() + offset,
+            lng: location.lng() + offset,
+          },
+          title: eventName,
         });
-
+        // InfoWindow associated with the Marker
         const infoWindow = new google.maps.InfoWindow({
           content: 
           `<div>
-          <h3>${locationName}</h3>
-          <p>${description}</p>
+          <h1>Event: ${eventName}</h1>
+          <h3>Location: ${locationName}</h3>
+          <p>Time: ${time}</p>
           </div>`,
         });
-
+        // InfoWindow opened when Marker is clicked
         marker.addListener('click', () => {
           infoWindow.open(map, marker);
         });
       }
     });
-  }, [google, map, locationName, description]);
+  }, [google, map, eventName, locationName, time]);
 
   return null;
 };
--- client/src/map.js
+++ client/src/map.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
+import {Map, GoogleApiWrapper, Marker, InfoWindow} from 'google-maps-react';
+import {collection, getDocs} from 'firebase/firestore';
 
 import CustomMarker from './CustomMarker';
+import {db} from './config/firebase';
 
-
+// Sets Style for Map
 const mapStyles = {
   width: '100%',
   height: '100%'
 };
 
+// Image used for User Location
 const userImage = 'https://cdn-icons-png.flaticon.com/512/1673/1673221.png';
 
+// Size of User Location Marker
 const markerSize = {
   width: 40,
   height: 40
 };
 
+// Center coordinates of UCSC to place Map
 const UCSCMID = {
   lat: 36.99548112809275,
   lng: -122.06084628167693
@@ -23,35 +28,53 @@ const UCSCMID = {
 
 class MapContainer extends Component {
   state = {
+    // Initialize lat, lng for userLocation
     userLocation: {
       lat: null,
       lng: null
     },
+    // Initialize InfoWindow to closed
     isInfoWindowOpen: {
-      currentLocation: false,
-    }
+      currentLocation: false
+    },
+    // Array to store Custom Markers
+    markers: [] 
   };
 
-  // Gets user location right when map is created
+  // Called when Map is rendered
   componentDidMount() {
+    // Check if Geolocation is available in User's browser
     if (navigator.geolocation) {
-      // Watch user's position
+      // Watches the User's position
       this.watchId = navigator.geolocation.watchPosition(
+        // Success callback function for when User's position is found
         position => {
-          // Update the userLocation state with the new coordinates
+          // Extract latitude and longitude from User's position
           const {latitude, longitude} = position.coords;
+          // Update userLocation state with new coords
           this.setState({
             userLocation: { lat: latitude, lng: longitude }
           });
         },
+        // Error callback function
         error => console.log(error),
-        // Additional options for geolocation
-        { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+        {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
       );
     }
-
+    // Get data from Firestare and add markers to state
+    getDocs(collection(db, 'events'))
+      .then(querySnapshot => {
+        // Extract data from querySnapshot and make an array of markers
+        const markers = querySnapshot.docs.map(doc => doc.data());
+        // Update the markers state with the data from markers array
+        this.setState({markers});
+      })
+      .catch(error => {
+        console.log('Error getting events:', error);
+      });
   }
 
+  // Update the isInfoWindowOpen state to open markerName info window
   handleMarkerClick = markerName => {
     this.setState(prevState => ({
       isInfoWindowOpen: {
@@ -61,7 +84,7 @@ class MapContainer extends Component {
     }));
   };
 
-  // Handle closing markers
+  // Update the isInfoWindowOpen state to close the markerName info window
   handleInfoWindowClose = markerName => {
     this.setState(prevState => ({
       isInfoWindowOpen: {
@@ -71,6 +94,7 @@ class MapContainer extends Component {
     }));
   };
 
+  // Clear the geolocation watch if it is active when the Map not being rendered
   componentWillUnmount() {
     if (navigator.geolocation) {
       navigator.geolocation.clearWatch(this.watchId);
@@ -78,19 +102,22 @@ class MapContainer extends Component {
   }
 
   render() {
+    // Defining variables from Map state
+    const {userLocation, isInfoWindowOpen, markers} = this.state;
 
-    const {userLocation, isInfoWindowOpen} = this.state;
-
+    // If User's Location isn't given then return message
     if (!userLocation.lat || !userLocation.lng) {
-      return <div>Loading...</div>;
+      return (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+          <h2>Share Location to View Map</h2>
+        </div>
+      );
     }
 
+    // Marker style for userLocation
     const markerImageStyle = {
       url: userImage,
-      scaledSize: new this.props.google.maps.Size(
-        markerSize.width,
-        markerSize.height
-      ),
+      scaledSize: new this.props.google.maps.Size(markerSize.width, markerSize.height),
       opacity: 0.8
     };
 
@@ -103,6 +130,7 @@ class MapContainer extends Component {
         className="full-screen-map"
         ref={map => (this.map = map)}
       >
+        {/*Marker for User's Location*/}
         <Marker
           position={userLocation}
           icon={markerImageStyle}
@@ -116,16 +144,20 @@ class MapContainer extends Component {
         >
           <div>
             <h3>Current Location</h3>
-            <p>Details here :)</p>
           </div>
         </InfoWindow>
 
-        <CustomMarker
-          google={this.props.google}
-          map={this.props.map}
-          locationName="Mchenry Library"
-          description="This is Kresge College"
-        />
+        {/* Render the Custom Markers */}
+        {markers.map((marker, index) => (
+          <CustomMarker
+            key={index}
+            google={this.props.google}
+            map={this.props.map}
+            eventName={marker.account}
+            locationName={marker.eventLocation}
+            time={marker.eventStartTime}
+          />
+        ))}
       </Map>
     );
   }
