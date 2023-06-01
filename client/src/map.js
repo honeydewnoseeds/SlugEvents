4718a84276488f2ca081ad78826fb46b694ee1ae
Edison <ediesonchen@gmail.com>
2023-06-01 15:45:10 -0700

    fix mobile changes

--- client/src/map.js
+++ client/src/map.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
+import {Map, GoogleApiWrapper, Marker, InfoWindow} from 'google-maps-react';
+import {collection, getDocs} from 'firebase/firestore';
 
+import CustomMarker from './CustomMarker';
+import {db} from './config/firebase';
+
+// Sets Style for Map
 const mapStyles = {
   width: '100%',
   height: '100%'
 };
 
-const image = 'https://cdn-icons-png.flaticon.com/512/1673/1673221.png';
+// Image used for User Location
+const userImage = 'https://cdn-icons-png.flaticon.com/512/1673/1673221.png';
 
+// Size of User Location Marker
 const markerSize = {
   width: 40,
   height: 40
 };
 
+// Center coordinates of UCSC to place Map
 const UCSCMID = {
   lat: 36.99548112809275,
   lng: -122.06084628167693
@@ -20,56 +28,73 @@ const UCSCMID = {
 
 class MapContainer extends Component {
   state = {
+    // Initialize lat, lng for userLocation
     userLocation: {
       lat: null,
       lng: null
     },
+    // Initialize InfoWindow to closed
     isInfoWindowOpen: {
-      currentLocation: false,
-      OAKES: false,
-      NAMASTE: false
+      currentLocation: false
     },
-    geocodedMarkers: {
-      OAKES: null,
-      NAMASTE: null
-    },
+    // Array to store Custom Markers
+    markers: [] 
   };
 
-  // Gets user location right when map is created
+  // Called when Map is rendered
   componentDidMount() {
+    // Check if Geolocation is available in User's browser
     if (navigator.geolocation) {
-      // Watch user's position
+      // Watches the User's position
       this.watchId = navigator.geolocation.watchPosition(
+        // Success callback function for when User's position is found
         position => {
-          // Update the userLocation state with the new coordinates
-          const { latitude, longitude } = position.coords;
+          // Extract latitude and longitude from User's position
+          const {latitude, longitude} = position.coords;
+          // Update userLocation state with new coords
           this.setState({
-            userLocation: { lat: latitude, lng: longitude }
+            userLocation: {lat: latitude, lng: longitude}
           });
         },
+        // Error callback function
         error => console.log(error),
-        // Additional options for geolocation
-        { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+        {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
       );
     }
-
-    // Perform geocoding for markers
-    this.geocodeMarker('OAKES', 'Oakes College, Santa Cruz, CA 95064');
-    this.geocodeMarker('NAMASTE', 'Namaste Lounge, Santa Cruz, CA 95064');
+    // Get data from Firestare and add markers to state
+    getDocs(collection(db, 'events'))
+      .then(querySnapshot => {
+        // Extract data from querySnapshot and make an array of markers
+        const markers = querySnapshot.docs.map(doc => doc.data());
+        // Update the markers state with the data from markers array
+        this.setState({markers});
+      })
+      .catch(error => {
+        console.log('Error getting events:', error);
+      });
   }
 
-  geocodeMarker = async (markerName, locationName) => {
-    // Geocoding logic...
-  };
-
+  // Update the isInfoWindowOpen state to open markerName info window
   handleMarkerClick = markerName => {
-    // Marker click handler...
+    this.setState(prevState => ({
+      isInfoWindowOpen: {
+        ...prevState.isInfoWindowOpen,
+        [markerName]: true
+      }
+    }));
   };
 
+  // Update the isInfoWindowOpen state to close the markerName info window
   handleInfoWindowClose = markerName => {
-    // InfoWindow close handler...
+    this.setState(prevState => ({
+      isInfoWindowOpen: {
+        ...prevState.isInfoWindowOpen,
+        [markerName]: false
+      }
+    }));
   };
 
+  // Clear the geolocation watch if it is active when the Map not being rendered
   componentWillUnmount() {
     if (navigator.geolocation) {
       navigator.geolocation.clearWatch(this.watchId);
@@ -77,18 +102,22 @@ class MapContainer extends Component {
   }
 
   render() {
-    const { userLocation, isInfoWindowOpen, geocodedMarkers } = this.state;
+    // Defining variables from Map state
+    const {userLocation, isInfoWindowOpen, markers} = this.state;
 
+    // If User's Location isn't given then return message
     if (!userLocation.lat || !userLocation.lng) {
-      return <div>Loading...</div>;
+      return (
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+          <h2>Share Location to View Map</h2>
+        </div>
+      );
     }
 
+    // Marker style for userLocation
     const markerImageStyle = {
-      url: image,
-      scaledSize: new this.props.google.maps.Size(
-        markerSize.width,
-        markerSize.height
-      ),
+      url: userImage,
+      scaledSize: new this.props.google.maps.Size(markerSize.width, markerSize.height),
       opacity: 0.8
     };
 
@@ -101,6 +130,7 @@ class MapContainer extends Component {
         className="full-screen-map"
         ref={map => (this.map = map)}
       >
+        {/*Marker for User's Location*/}
         <Marker
           position={userLocation}
           icon={markerImageStyle}
@@ -114,45 +144,20 @@ class MapContainer extends Component {
         >
           <div>
             <h3>Current Location</h3>
-            <p>Details here :)</p>
           </div>
         </InfoWindow>
 
-        {geocodedMarkers.OAKES && (
-          <Marker
-            position={geocodedMarkers.OAKES}
-            title={'OAKES'}
-            onClick={() => this.handleMarkerClick('OAKES')}
+        {/* Render the Custom Markers */}
+        {markers.map((marker, index) => (
+          <CustomMarker
+            key={index}
+            google={this.props.google}
+            map={this.props.map}
+            eventName={marker.account}
+            locationName={marker.eventLocation}
+            time={marker.eventStartTime}
           />
-        )}
-        <InfoWindow
-          position={geocodedMarkers.OAKES}
-          visible={isInfoWindowOpen.OAKES}
-          onClose={() => this.handleInfoWindowClose('OAKES')}
-        >
-          <div>
-            <h3>OAKES</h3>
-            <p>Details here :)</p>
-          </div>
-        </InfoWindow>
-
-        {geocodedMarkers.NAMASTE && (
-          <Marker
-            position={geocodedMarkers.NAMASTE}
-            title={'NAMASTE'}
-            onClick={() => this.handleMarkerClick('NAMASTE')}
-          />
-        )}
-        <InfoWindow
-          position={geocodedMarkers.NAMASTE}
-          visible={isInfoWindowOpen.NAMASTE}
-          onClose={() => this.handleInfoWindowClose('NAMASTE')}
-        >
-          <div>
-            <h3>NAMASTE</h3>
-            <p>Details here :)</p>
-          </div>
-        </InfoWindow>
+        ))}
       </Map>
     );
   }
