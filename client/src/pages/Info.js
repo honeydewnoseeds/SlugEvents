c2eb38c45aed2dac443781705189c7f5f628638a
yunqing liang <yliang82@ucsc.edu>
2023-06-06 15:22:18 -0700

    fixed info

--- client/src/pages/Info.js
+++ client/src/pages/Info.js
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-import {
-  Stack,
-  Button,
-  Box,
-  IconButton,
-  Grid,
-  CardMedia,
-  useMediaQuery,
-} from "@mui/material";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
-import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
-import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
-import { useTheme } from "@mui/material/styles";
-import Card from "@mui/material/Card";
-import MobileStepper from "@mui/material/MobileStepper";
-import Typography from "@mui/material/Typography";
-=======
 import { Stack, Button, Box, IconButton, Grid, CardMedia, useMediaQuery} from "@mui/material";
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
@@ -26,15 +7,10 @@ import { useTheme } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import MobileStepper from '@mui/material/MobileStepper';
 import Typography from '@mui/material/Typography';
->>>>>>> origin/charlotte
 import { useLocation, useNavigate } from "react-router-dom";
-import * as React from "react";
+import * as React from 'react';
 
-<<<<<<< HEAD
-function Board() {
-=======
 function Info() {
->>>>>>> origin/charlotte
   const navigate = useNavigate();
   const location = useLocation();
   // dynamic theme 
@@ -53,7 +29,7 @@ function Info() {
 
   const handleBack = () => {
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
+  };    
 
   // steps variables
   const [activeStep, setActiveStep] = React.useState(0);
@@ -64,21 +40,9 @@ function Info() {
   // if current page = 0 : displays the image dynamically
   // if current page = 1 : displays the descriptions
   const slider = (current) => {
-    if (current === 0) {
-      return (
+    if (current===0){
+      return (   
         <CardMedia>
-<<<<<<< HEAD
-          <img
-            class="vertical-center"
-            src={imageSrc}
-            alt="titlee"
-            style={{
-              width: isSmallScreen ? "90%" : "50%",
-              maxHeight: "74vh", // Set a fixed height for the images
-              objectFit: "contain",
-              borderRadius: "16px, 16px, 16px, 16px",
-            }}
-=======
           <img class="vertical-center"
                 src= {imageSrc}
                 alt= "titlee"
@@ -88,36 +52,24 @@ function Info() {
                   objectFit: "contain",
                   borderRadius: "16px, 16px, 16px, 16px",
                 }}
->>>>>>> origin/charlotte
           />
-        </CardMedia>
-      );
-    } else {
-      return (
-        <CardMedia>
-          <Typography variant="h6">{description}</Typography>
-        </CardMedia>
-      );
+          </CardMedia>
+      );          
+    }else{
+        return( 
+          <CardMedia>
+            <Typography variant= "h6">
+              {description}
+            </Typography>
+          </CardMedia>
+
+        );    
     }
-  };
+  }
 
   return (
+
     <div
-<<<<<<< HEAD
-      style={{
-        display: "flex",
-        alignItems: "center",
-        textAlign: "center",
-        height: "100%",
-      }}
-    >
-      <Box
-        minHeight="100vh"
-        width="auto"
-        height="auto"
-        //auto fit constraints
-        flexGrow={1}
-=======
     style={{
       display: "flex",
       alignItems: "center",
@@ -132,74 +84,10 @@ function Info() {
         height="auto"
         flexGrow={1}
         // setting the colors of the background
->>>>>>> origin/charlotte
         sx={{
           position: "relative",
           display: "flex",
           flexDirection: "column",
-<<<<<<< HEAD
-          //justifyContent: "center",
-          alignItems: "center",
-          backgroundColor: "background.default",
-        }}
-      >
-        <Grid container justifyContent="flex-start">
-          <IconButton onClick={() => navigate("/landing")}>
-            <ArrowBackIcon />
-          </IconButton>
-        </Grid>
-
-        <Card
-          variant="outlined"
-          sx={{
-            border: "primary.main",
-            borderRadius: "16px",
-            width: "80vw",
-            height: "78vh",
-            display: isSmallScreen ? "flex" : "",
-            alignItems: "center",
-            justifyContent: "center",
-            p: 2,
-          }}
-        >
-          {slider(activeStep)}
-        </Card>
-
-        <MobileStepper
-          steps={maxSteps}
-          position="static"
-          activeStep={activeStep}
-          nextButton={
-            <Button
-              size="small"
-              onClick={handleNext}
-              disabled={activeStep === maxSteps - 1}
-            >
-              Next
-              {theme.direction === "rtl" ? (
-                <KeyboardArrowLeft />
-              ) : (
-                <KeyboardArrowRight />
-              )}
-            </Button>
-          }
-          backButton={
-            <Button
-              size="small"
-              onClick={handleBack}
-              disabled={activeStep === 0}
-            >
-              {theme.direction === "rtl" ? (
-                <KeyboardArrowRight />
-              ) : (
-                <KeyboardArrowLeft />
-              )}
-              Back
-            </Button>
-          }
-        />
-
-=======
           alignItems: "center",
           backgroundColor: "background.default",
         }}
@@ -251,7 +139,6 @@ function Info() {
             }
         />
 
->>>>>>> origin/charlotte
         <Stack>
           <Button variant="contained" startIcon={<CalendarMonthIcon />}>
             Calendar
@@ -260,6 +147,6 @@ function Info() {
       </Box>
     </div>
   );
-}
+};
 
 export default Info;
